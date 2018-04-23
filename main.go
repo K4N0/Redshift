a4c08bbfca692e6c6172f1b9b86884f4fdd90f80
K4N0 <bad_cholo@yahoo.com>
2018-04-22 18:59:00 -0700

    initial upload

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	_ "github.com/gorilla/websocket"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	http.ListenAndServe(":80", nil)
+}
